5a36e8075f38155208ad1dc014cf9c2a9a356dd2
Taiho1998 <tgo12323@gmail.com>
2024-11-11 15:19:58 +0900

    14

--- workspace/ch01-buildup/react/05/src/App.jsx
+++ workspace/ch01-buildup/react/05/src/App.jsx
@@ -4,42 +4,40 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
-  return function App() {
-    const [itemList, setItemList] = useState([
-      { _id: 1, title: "두부", done: true },
-      { _id: 2, title: "계란", done: false },
-      { _id: 3, title: "라면", done: true },
-    ]);
+  const [itemList, setItemList] = useState([
+    { _id: 1, title: "두부", done: true },
+    { _id: 2, title: "계란", done: false },
+    { _id: 3, title: "라면", done: true },
+  ]);
 
-    const addItem = (item) => {
-      const newItemList = [...itemList, item];
-      setItemList(newItemList);
-    };
-
-    const toggleDone = (_id) => {
-      const item = itemList.find((item) => item._id === _id);
-      item.done = !item.done;
-      setItemList([...itemList]);
-    };
-
-    const deleteItem = (_id) => {
-      const newItemList = itemList.filter((item) => item._id !== _id);
-      setItemList(newItemList);
-    };
-
-    return (
-      <div id="todo">
-        <Header />
-        <Todo
-          itemList={itemList}
-          addItem={addItem}
-          toggleDone={toggleDone}
-          deleteItem={deleteItem}
-        />
-        <Footer />
-      </div>
-    );
+  const addItem = (item) => {
+    const newItemList = [...itemList, item];
+    setItemList(newItemList);
   };
+
+  const toggleDone = (_id) => {
+    const item = itemList.find((item) => item._id === _id);
+    item.done = !item.done;
+    setItemList([...itemList]);
+  };
+
+  const deleteItem = (_id) => {
+    const newItemList = itemList.filter((item) => item._id !== _id);
+    setItemList(newItemList);
+  };
+
+  return (
+    <div id="todo">
+      <Header />
+      <Todo
+        itemList={itemList}
+        addItem={addItem}
+        toggleDone={toggleDone}
+        deleteItem={deleteItem}
+      />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
