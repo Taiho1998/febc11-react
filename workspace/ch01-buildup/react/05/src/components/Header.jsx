78418edf9ab7f7012b91fb1dde295c721b81e772
Taiho1998 <tgo12323@gmail.com>
2024-11-12 10:05:39 +0900

    15

--- workspace/ch01-buildup/react/05/src/components/Header.jsx
+++ workspace/ch01-buildup/react/05/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1>Todo List - 컴포넌트 모듈화</h1>
+      <h1>Todo List - 컴포넌트의 모듈화 :()</h1>
       <p>
         파일 경로:{" "}
         <span id="filepath">{`ch${
