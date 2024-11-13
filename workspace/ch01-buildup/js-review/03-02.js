ff321e1e20f2021a8fde37ac97f377c6eeac9900
Taiho1998 <tgo12323@gmail.com>
2024-11-13 15:31:14 +0900

    19

--- workspace/ch01-buildup/js-review/03-02.js
+++ workspace/ch01-buildup/js-review/03-02.js
@@ -1,2 +1,19 @@
-var item = { no: 1, todo: '두부', done: true };
+var item = { no: 1, todo: "두부", done: true };
 
+// 1. 대입 연산자로 newItem 생성
+var newItem = item;
+
+// 2. Object.assign() 사용해서 속성 추가
+// Object.assign()(target, ...source); target 객체에 source 객체의 속성을 추가하여 target을 반환
+var newItem = Object.assign(item, { delete: true });
+var newItem = Object.assign({}, item, { delete: true }); // 위와 다르게 새로운 메모리의 객체를 생성한다는 뜻
+
+// 3. item의 속성으로 새로운 객체 생성
+var newItem = { no: item.no, done: item.done, todo: item.todo };
+
+// 4. 전개 연산자를 이용한 복사
+var newItem = { ...item, done: false };
+
+// newItem.done = false;
+console.log(item, newItem);
+console.log(item === newItem);
--- workspace/ch01-buildup/js-review/03-04.js
+++ workspace/ch01-buildup/js-review/03-04.js
@@ -1,6 +1,22 @@
 var itemList = [
-  { no: 1, todo: '두부', done: false},
-  { no: 2, todo: '계란', done: false},
-  { no: 3, todo: '라면', done: false},
+  { no: 1, todo: "두부", done: false },
+  { no: 2, todo: "계란", done: false },
+  { no: 3, todo: "라면", done: false },
 ];
 
+var newItemList = itemList;
+
+// 2. 전개 연산자를 이용한 복사(얕은 복사)
+// 주소는 바뀌었지만, 같은 사람임
+// 리액트에서의 상태의 불변성에 어긋남
+var newItemList = [...itemList];
+
+// 3. 객체를 속성으로 가질 경우 깊은 복사를 위해서는 속성도 새로운 객체로 복사해야 함
+// 불변성을 지키기 위함
+newItemList[1] = { ...itemList[1] };
+
+newItemList[1].done = true;
+
+console.log(itemList, newItemList);
+console.log(itemList === newItemList);
+console.log(itemList[1] === newItemList[1]);
