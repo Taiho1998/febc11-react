affc63d78646a9c7840b4ade0b84a8c623c645b5
Taiho1998 <tgo12323@gmail.com>
2024-11-13 11:04:14 +0900

    18

---
+++ workspace/ch02-start/props/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>Counter - React</h1>
+      <p>
+        파일 경로: <span id="filepath"></span>
+      </p>
+    </header>
+  );
+}
+
+export default Header;
