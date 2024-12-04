4d46334ecb6b9e01739ec6c207de059c6ebff464
Taiho1998 <tgo12323@gmail.com>
2024-12-04 10:45:54 +0900

    45

--- workspace/ch09-ajax/03-nike-reactquery/src/App.jsx
+++ workspace/ch09-ajax/03-nike-reactquery/src/App.jsx
@@ -4,37 +4,49 @@ import { DotLoader } from "react-spinners";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import { ToastContainer } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
   console.log("App 렌더링");
 
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [data, setData] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
   const axios = useAxiosInstance();
 
-  const fetchData = async (_id) => {
-    setIsLoading(true);
-    try {
-      const res = await axios.get(`/products/${_id} `, {
-        params: { delay: 1000 },
-      });
-      // const res = await axios.get(`/products/${_id} `);
-      setData(res.data.item);
-      setError(null);
-    } catch (err) {
-      console.error(err);
-      setError({ message: "잠시 후 다시 요청하십시오" });
-      setData(null);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const fetchData = async (_id) => {
+  //   setIsLoading(true);
+  //   try {
+  //     const res = await axios.get(`/products/${_id} `, {
+  //       params: { delay: 1000 },
+  //     });
+  //     // const res = await axios.get(`/products/${_id} `);
+  //     setData(res.data.item);
+  //     setError(null);
+  //   } catch (err) {
+  //     console.error(err);
+  //     setError({ message: "잠시 후 다시 요청하십시오" });
+  //     setData(null);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData(5);
-  }, []);
+  // useEffect(() => {
+  //   fetchData(5);
+  // }, []);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products", 7],
+    queryFn: () => axios.get(`/products/7`),
+    select: (res) => res.data.item,
+  });
+
+  console.log("isLoading", isLoading);
+  console.log("error", error);
+  console.log("data", data);
 
   const basicShippingFee = 3000;
 
@@ -55,7 +67,7 @@ function App() {
 
   return (
     <>
-      <h1>02 - Nike 상품 상세 조회 - axios</h1>
+      <h1>02 - Nike 상품 상세 조회 - React-Query </h1>
       {isLoading && <DotLoader />}
       {error && <p>{error.message}</p>}
       {data && (
